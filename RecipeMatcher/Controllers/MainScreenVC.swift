77a0c2702ede312c3955d141a73191450be3f2d2
Eric Widjaja <ericwidjaja@pursuit.org>
2019-11-19 19:39:12 -0500

    created logIn and password TextFields and their constraints onto MainVC

--- RecipeMatcher/Controllers/MainScreenVC.swift
+++ RecipeMatcher/Controllers/MainScreenVC.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import Foundation
 
 class MainScreenVC : UIViewController {
     
-    lazy var backgroundImageView : UIImageView = {
+    lazy var backgroundImageView: UIImageView = {
         let mainImage = UIImageView()
         mainImage.image = #imageLiteral(resourceName: "SplashScreen")
         mainImage.contentMode = .scaleAspectFill
@@ -18,7 +19,7 @@ class MainScreenVC : UIViewController {
         return mainImage
     }()
     
-    lazy var titleLabel : UILabel = {
+    lazy var titleLabel: UILabel = {
         let mainTitle = UILabel()
         mainTitle.adjustsFontSizeToFitWidth = true
         mainTitle.font = UIFont.init(name: "Noteworthy-Bold", size: 50)
@@ -28,20 +29,43 @@ class MainScreenVC : UIViewController {
         return mainTitle
     }()
     
+    lazy var loginIdTextField: UITextField = {
+        let logIn = UITextField()
+        logIn.placeholder = "Enter Email"
+        logIn.font = UIFont(name: "Noteworthy", size: 18)
+        logIn.borderStyle = .roundedRect
+        logIn.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        logIn.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        logIn.autocorrectionType = .no
+//        logIn.addTarget(self, action: #selector(validateFields), for: .editingChanged)
+        return logIn
+    }()
     
-    lazy var signInButton : UIButton = {
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Password"
+        textField.font = UIFont(name: "Noteworthy", size: 16)
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.autocorrectionType = .no
+        textField.isSecureTextEntry = true
+//        textField.addTarget(self, action: #selector(validateFields), for: .editingChanged)
+        return textField
+    }()
+    
+    lazy var signInButton: UIButton = {
         let signIn = UIButton()
         signIn.setTitle("Sign In", for: .normal)
-        signIn.layer.cornerRadius = 5
+        signIn.layer.cornerRadius = 10
         signIn.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
         signIn.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         return signIn
     }()
     
-    lazy var createAcctButton : UIButton = {
+    lazy var createAcctButton: UIButton = {
         let createAcct = UIButton()
         createAcct.setTitle("Create Account", for: .normal)
-        createAcct.layer.cornerRadius = 5
+        createAcct.layer.cornerRadius = 10
         createAcct.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
         createAcct.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         return createAcct
@@ -57,6 +81,8 @@ class MainScreenVC : UIViewController {
     private func setMainViewConstraints() {
         backGroundImageConstraints()
         mainTitleConstraints()
+        loginIdConstraints()
+        passwordConstraints()
         signInConstraints()
         createAccountConstraints()
     }
@@ -70,20 +96,32 @@ class MainScreenVC : UIViewController {
         view.addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([titleLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor), titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40), titleLabel.heightAnchor.constraint(equalToConstant: 70), titleLabel.widthAnchor.constraint(equalToConstant: 350)])
+        NSLayoutConstraint.activate([titleLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor), titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60), titleLabel.heightAnchor.constraint(equalToConstant: 70), titleLabel.widthAnchor.constraint(equalToConstant: 350)])
+    }
+    
+    private func loginIdConstraints() {
+        view.addSubview(loginIdTextField)
+        loginIdTextField.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([loginIdTextField.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.centerYAnchor), loginIdTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor), loginIdTextField.heightAnchor.constraint(equalToConstant: 35), loginIdTextField.widthAnchor.constraint(equalToConstant: 250)])
+    }
+    
+    private func passwordConstraints() {
+        view.addSubview(passwordTextField)
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([passwordTextField.topAnchor.constraint(equalTo: loginIdTextField.bottomAnchor, constant: 10), passwordTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor), passwordTextField.heightAnchor.constraint(equalToConstant: 35), passwordTextField.widthAnchor.constraint(equalToConstant: 250)])
     }
     
     private func signInConstraints() {
         view.addSubview(signInButton)
         signInButton.translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([signInButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 425), signInButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor), signInButton.heightAnchor.constraint(equalToConstant: 24), signInButton.widthAnchor.constraint(equalToConstant: 150)])
+        NSLayoutConstraint.activate([signInButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 450), signInButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor), signInButton.heightAnchor.constraint(equalToConstant: 30), signInButton.widthAnchor.constraint(equalToConstant: 150)])
     }
     
     private func createAccountConstraints() {
         view.addSubview(createAcctButton)
         createAcctButton.translatesAutoresizingMaskIntoConstraints = false
         
-        NSLayoutConstraint.activate([createAcctButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 10), createAcctButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor), createAcctButton.heightAnchor.constraint(equalToConstant: 24), createAcctButton.widthAnchor.constraint(equalToConstant: 150)])
+        NSLayoutConstraint.activate([createAcctButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 10), createAcctButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor), createAcctButton.heightAnchor.constraint(equalToConstant: 30), createAcctButton.widthAnchor.constraint(equalToConstant: 150)])
     }
 }
--- RecipeMatcher/SceneDelegate.swift
+++ RecipeMatcher/SceneDelegate.swift
@@ -20,7 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = createMainTabBarController()
+//        window?.rootViewController = createMainTabBarController()
+        window?.rootViewController = MainScreenVC ()
         window?.makeKeyAndVisible()
     }
     
