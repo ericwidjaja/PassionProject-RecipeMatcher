2c4a106b691dbc2c4614f3ff730f615fb9f6d314
Eric Widjaja <ericwidjaja@pursuit.org>
2019-12-23 23:04:29 -0500

    create logIn func to firebaseAuth

--- RecipeMatcher/Controllers/LogInScreenVC.swift
+++ RecipeMatcher/Controllers/LogInScreenVC.swift
@@ -3,9 +3,8 @@
 
 import UIKit
 import FirebaseAuth
-import FirebaseFirestore
 
-class LogInScreenVC : UIViewController {
+class LogInScreenVC: UIViewController {
     
     var logInEmail: UITextField?
     var logInPassword: UITextField?
@@ -13,11 +12,9 @@ class LogInScreenVC : UIViewController {
     
     private func createMainTabBarController() -> UITabBarController {
         let navController = UINavigationController(rootViewController: SearchRecipesVC())
-        
         let navController2 = UINavigationController(rootViewController: RecipesCollResultsVC())
-
+        
         navController.tabBarItem = UITabBarItem(title: "SEARCH", image: UIImage(systemName: "magnifyingglass.circle"), tag: 0)
-
         navController2.tabBarItem = UITabBarItem(title: "COLLECTION", image: UIImage(systemName: "folder.fill.badge.plus"), tag: 1)
         
         let tabVC = UITabBarController()
@@ -25,36 +22,84 @@ class LogInScreenVC : UIViewController {
         return tabVC
     }
     
+    private func showAlert(with title: String, and message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alertVC, animated: true, completion: nil)
+    }
+    //MARK: - OBJ-C Methods
+    
+    @objc func validateFields() {
+        guard logInEmail!.hasText, logInPassword!.hasText else {
+            logInScreen.signInButton.isEnabled = false
+            return }
+        logInScreen.signInButton.isEnabled = true
+    }
+    
+    @objc func signInButtonPressed() {
+        let signInButton = logInScreen.signInButton
+        guard let email = logInScreen.loginIdTextField.text, !email.isEmpty,
+            let password = logInScreen.passwordTextField.text, !password.isEmpty
+            else {
+                showAlert(with: "Required", and: "Please fill out all fields")
+                return
+        }
+        FirebaseAuthService.manager.loginUser(email: email.lowercased(), password: password) { (result) in self.handleSignInResponse(with: result)
+        }
+    }
+    //MARK: Firebase Authentication Methods
+    private func handleSignInResponse(with result: Result<(), Error>) {
+        switch result {
+        case .failure(let error):
+            print(error)
+            self.showAlert(with: "Error", and: "Could not log in. Error: \(error)")
+            
+        case .success:
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                let sceneDelegate = windowScene.delegate as? SceneDelegate, let window = sceneDelegate.window
+                else { return }
+            print("SignIn Successful")
+            UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromBottom, animations: {
+                window.rootViewController = {
+                    let mainVC = RecipeMatchTabBar()
+                    return mainVC
+                }()
+                
+            }, completion: nil)
+        }
+    }
+    
+    @objc func createAcctPressed() {
+//        let createAcct = CreateAcctVC()
+//        createAcct.modalPresentationStyle = .currentContext
+//        present(createAcct, animated: true, completion: nil)
+    print("createAcctIsPressed")
+    }
+    
     @objc func skipButtonPressed() {
-        //complete implementation
-//       let destination = RecipeMatchTabBar()
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
             let sceneDelegate = windowScene.delegate as? SceneDelegate, let window = sceneDelegate.window
             else {
                 return }
         window.rootViewController = {
             let searchSetUpVC = RecipeMatchTabBar()
-                searchSetUpVC.selectedIndex = 0
+            searchSetUpVC.selectedIndex = 0
             return searchSetUpVC
         }()
     }
     
-    
-    
-//MARK: - Lifecycle
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(logInScreen)
         logInEmail?.delegate = self
         logInPassword?.delegate = self
-        logInScreen.skipButton.addTarget(self, action: #selector(skipButtonPressed), for: .touchUpInside)
         
-//        logInScreen.signInButton.addTarget(self, action: #selector(signInButtonPressed), for: .touchUpInside)
-//        
-//        logInScreen.createNewAcctButton.addTarget(self, action: #selector(createAcctForm), for: .touchUpInside)
+        logInScreen.signInButton.addTarget(self, action: #selector(signInButtonPressed), for: .touchUpInside)
+        logInScreen.createAcctButton.addTarget(self, action: #selector(createAcctPressed), for: .touchUpInside)
+        logInScreen.skipButton.addTarget(self, action: #selector(skipButtonPressed), for: .touchUpInside)
     }
 }
-
 
 //MARK: - Extensions
 extension LogInScreenVC: UITextFieldDelegate {
