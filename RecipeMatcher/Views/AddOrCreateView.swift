5f6906ee450a027029556a8b0cd4c00b954e1e8b
Eric Widjaja <ericwidjaja@pursuit.org>
2020-06-27 20:47:28 -0400

    set constraints on addOrCreateView

--- RecipeMatcher/Views/AddOrCreateView.swift
+++ RecipeMatcher/Views/AddOrCreateView.swift
@@ -15,12 +15,12 @@ class AddOrCreateView: UIView {
         let button = UIButton()
         button.tintColor = .green
         button.isUserInteractionEnabled = true
-        let config = UIImage.SymbolConfiguration(pointSize: 35, weight: UIImage.SymbolWeight.medium)
+        let config = UIImage.SymbolConfiguration(pointSize: 40, weight: UIImage.SymbolWeight.medium)
         let createCollectionFolder = UIImage(systemName: "folder.fill.badge.plus", withConfiguration: config)
         button.setImage(createCollectionFolder, for: .normal)
         var newFrame = button.frame.size
-        newFrame.width = 35
-        newFrame.height = 35
+        newFrame.width = 40
+        newFrame.height = 40
         button.frame.size = newFrame
         return button
     }()
@@ -29,13 +29,13 @@ class AddOrCreateView: UIView {
         let button = UIButton()
         button.tintColor = .green
         button.isUserInteractionEnabled = true
-        let config = UIImage.SymbolConfiguration(pointSize: 35, weight: UIImage.SymbolWeight.medium)
+        let config = UIImage.SymbolConfiguration(pointSize: 40, weight: UIImage.SymbolWeight.medium)
         let addToCollection = UIImage(systemName: "plus", withConfiguration: config)
         button.setImage(addToCollection, for: .normal)
         //bookmark.fill?
         var newFrame = button.frame.size
-        newFrame.width = 35
-        newFrame.height = 35
+        newFrame.width = 40
+        newFrame.height = 40
         button.frame.size = newFrame
         return button
     }()
@@ -67,10 +67,51 @@ class AddOrCreateView: UIView {
     }
     
     private func makeAlert() {
-        let alert = UIAlertController(title: "Required", message: "Enter a name for new collection", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert.addAction(ok)
-        present(alert, animated: true, completion: nil)
+        //        let alert = UIAlertController(title: "Required", message: "Enter a name for new collection", preferredStyle: .alert)
+        //        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        //        alert.addAction(ok)
+        //        present(alert, animated: true, completion: nil)
     }
-
+    
+    //MARK: - Overrides
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        addOrCreateConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    //MARK: - Constraints
+    private func addOrCreateConstraints() {
+        addViewsToSubView()
+        createCollectionButtonConstraints()
+        addToCollectionButtonConstraints()
+        newCollectionTextFieldConstraints()
+        
+    }
+    private func createCollectionButtonConstraints() {
+        NSLayoutConstraint.activate([
+            createCollectionButton.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+            createCollectionButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            createCollectionButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            createCollectionButton.heightAnchor.constraint(equalToConstant: 70)])
+        }
+    
+    private func addToCollectionButtonConstraints() {
+        NSLayoutConstraint.activate([
+            addToCollectionButton.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+            addToCollectionButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            addToCollectionButton.topAnchor.constraint(equalTo: createCollectionButton.bottomAnchor, constant: 30),
+            addToCollectionButton.heightAnchor.constraint(equalToConstant: 70)])
+    }
+    
+    private func newCollectionTextFieldConstraints() {
+        NSLayoutConstraint.activate([
+        newCollectionTextField.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+        newCollectionTextField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+        newCollectionTextField.topAnchor.constraint(equalTo: addToCollectionButton.bottomAnchor, constant: 50),
+        newCollectionTextField.heightAnchor.constraint(equalToConstant: 70)])
+        }
 }
