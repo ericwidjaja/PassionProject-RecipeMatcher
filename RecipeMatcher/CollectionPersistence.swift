bcd281943905fba5015f9314269b5476e7a6598b
Eric Widjaja <ericwidjaja@pursuit.org>
2020-08-10 21:56:13 -0400

    Create Persistence Collection File

---
+++ RecipeMatcher/CollectionPersistence.swift
@@ -0,0 +1,23 @@
+//  CollectionPersistence.swift
+//  RecipeMatcher
+//  Created by Eric Widjaja on 8/10/20.
+//  Copyright © 2020 Eric W. All rights reserved.
+
+import Foundation
+
+class CollectionPersistence {
+    private init() {}
+    static let helper = CollectionPersistence()
+    private let persistanceHelper = PersistenceHelper<FaveCollections>.init(fileName: "FaveCollections.plist")
+    
+    func save(newElement: FaveCollections) throws {
+        try persistanceHelper.save(newElement: newElement)
+    }
+    func replace(newArr: [FaveCollections]) throws {
+        try persistanceHelper.replace(arrOfElements: newArr)
+    }
+    
+    func getRecipe() throws -> [FaveCollections] {
+        try persistanceHelper.getObjects()
+    }
+}
