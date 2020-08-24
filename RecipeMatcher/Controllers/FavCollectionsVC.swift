7d64cd9dacc4b5a7dd3cc9e775ff7c5353175ca6
Eric Widjaja <ericwidjaja@pursuit.org>
2020-08-23 20:50:47 -0400

    create some constraints to show 'My Collections' title in FavCollVC

--- RecipeMatcher/Controllers/FavCollectionsVC.swift
+++ RecipeMatcher/Controllers/FavCollectionsVC.swift
@@ -9,22 +9,14 @@
 import UIKit
 
 class FavCollectionsVC: UIViewController {
+    
+    //MARK: - Properties
+    var favCollView = FavCollectionsView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(favCollView)
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- RecipeMatcher/Views/FavCollectionsView.swift
+++ RecipeMatcher/Views/FavCollectionsView.swift
@@ -7,8 +7,39 @@
 //
 
 import UIKit
+import Kingfisher
 
 class FavCollectionsView: UIView {
+    
+    lazy var favCollectionsTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "My Collections"
+        label.font = .boldSystemFont(ofSize: 40)
+        label.textColor = .white
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        constraintsFavCollectionsTitleLabel()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        constraintsFavCollectionsTitleLabel()
+    }
+    
+    //MARK: - FavCollectionsView Constraints
+    private func constraintsFavCollectionsTitleLabel(){
+        addSubview(favCollectionsTitleLabel)
+        favCollectionsTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            favCollectionsTitleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
+            favCollectionsTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            favCollectionsTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            favCollectionsTitleLabel.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
 
     /*
     // Only override draw() if you perform custom drawing.
