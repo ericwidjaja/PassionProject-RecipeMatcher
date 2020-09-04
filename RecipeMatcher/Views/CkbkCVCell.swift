27d26d902957179f0ce31fc4da1acf322da3a2af
Eric Widjaja <ericwidjaja@pursuit.org>
2020-09-03 20:43:28 -0400

    create CkbkCC cell

---
+++ RecipeMatcher/Views/CkbkCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  CkbkCVCell.swift
+//  RecipeMatcher
+//
+//  Created by Eric Widjaja on 9/3/20.
+//  Copyright © 2020 Eric W. All rights reserved.
+//
+
+import UIKit
+
+class CkbkCVCell: UICollectionViewCell {
+    
+}
