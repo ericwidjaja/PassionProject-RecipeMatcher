f10a5ddc7488828aec22c7bc3cbae69d292fa69f
Eric Widjaja <ericwidjaja@pursuit.org>
2020-09-03 20:54:42 -0400

    adjust constraints in CkbkCVCell and refactor to hide Navigation item button in SearchRecipesVC

--- RecipeMatcher/Controllers/SearchRecipesVC.swift
+++ RecipeMatcher/Controllers/SearchRecipesVC.swift
@@ -61,6 +61,18 @@ class SearchRecipesVC : UIViewController {
         loadSearchData()
         searchView.findRecipesButton.addTarget(self, action: #selector(findRecipeButtonPressed), for: .touchUpInside)
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Sign Out", style: .done, target: self, action: #selector(signOut))
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.navigationController?.navigationBar.isHidden = true
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        self.navigationController?.navigationBar.isHidden = false
     }
 }
 
--- RecipeMatcher/Views/CkbkCVCell.swift
+++ RecipeMatcher/Views/CkbkCVCell.swift
@@ -1,13 +1,35 @@
-//
 //  CkbkCVCell.swift
 //  RecipeMatcher
-//
 //  Created by Eric Widjaja on 9/3/20.
 //  Copyright © 2020 Eric W. All rights reserved.
-//
 
 import UIKit
+import Kingfisher
 
 class CkbkCVCell: UICollectionViewCell {
     
+    lazy var ckbkImage: UIImageView = {
+        let ckbkImg = UIImageView()
+        ckbkImg.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        return ckbkImg
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        ckbkImgConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CkbkCVCell {
+    func ckbkImgConstraints() {
+        contentView.addSubview(ckbkImage)
+        ckbkImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            ckbkImage.heightAnchor.constraint(equalTo: contentView.heightAnchor),
+            ckbkImage.widthAnchor.constraint(equalTo: contentView.widthAnchor)])
+    }
 }
